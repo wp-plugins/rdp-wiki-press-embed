42e8493d0508df776f824cfe44d0a43f981a9a36
rpayne7264 <rpayne7264@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-01 20:08:58 +0000

    Release 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/rdp-wiki-press-embed/tags/1.0.0@1036183 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rdp-wiki-press-embed/resources/js/script.wcr.js
@@ -0,0 +1,29 @@
+var $j=jQuery.noConflict();
+// Use jQuery via $j(...)
+
+$j(document).ready(rdp_wcr_main_onReady);
+function rdp_wcr_main_onReady(){
+    $j('#mainContent .ready').removeClass('invisible');
+    rdp_wcr_handle_links();
+}//rdp_wcr_main_onReady
+
+function rdp_wcr_handle_links(){
+    var baseURL = window.location.protocol + "//" + window.location.host + window.location.pathname;
+    var baseTarget = (typeof WikiEmbedSettings != 'undefined')? url('protocol', WikiEmbedSettings.target_url) + "//" + url('hostname', WikiEmbedSettings.target_url): '';
+    $j("a").each(function(i){
+        if($j(this).hasClass('ppe-add-to-cart'))return true;
+        if($j(this).hasClass('ppe-cover-link'))return true;        
+        var sHREF = $j(this).attr('href');
+
+        if(typeof sHREF == 'undefined')return true;        
+        if(sHREF.substring(0, 1) == '#')return true;
+        var n = rdp_wcr.domains.indexOf(url('domain', sHREF));
+        var p = 'pediapress.com'.indexOf(url('domain', sHREF));
+        if(n <= 0 && p <= 0)return true;
+        if(sHREF.substring(0, 2) == '//') sHREF = 'http:' + sHREF;
+        if(baseTarget != '' && sHREF.substring(0, 1) == '/') sHREF = baseTarget + sHREF;        
+        urls = sHREF.match(/(https?:\/\/[^\s]+)/g);
+        if(urls == null)return true;
+        $j(this).attr('href',baseURL+jQuery.query.set("wikiembed-override-url", sHREF) ).removeAttr('target');                
+    });     
+}//rdp_wcr_handle_links
